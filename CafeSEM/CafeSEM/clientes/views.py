81b092024547f666bb97c74e66695abffb027085
Eva Gómez <88188128+mogaves@users.noreply.github.com>
2024-12-09 10:09:29 +0100

    Update views.py
    Añado la funcion listado de productos

--- CafeSEM/CafeSEM/clientes/views.py
+++ CafeSEM/CafeSEM/clientes/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from clientes.models import Producto
 
 def index(request):
+    return render(request, "index.html")
 
-    return render(request, "clientes/index.html")
+def listadoProductos(request):
+    p = Producto()
+    listado = p.listadoProductos()
+
+    contexto = {
+        'listado': listado
+    }
+
+    return render(request, "listadoProductos.html", contexto)
