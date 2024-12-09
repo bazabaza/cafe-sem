ab4633a921e648ea8337a9d083ab9394354c73f8
Eva Gómez <88188128+mogaves@users.noreply.github.com>
2024-12-09 10:10:51 +0100

    Update models.py
    Añado la función listadoProductos para mostrar todos los productos de la tienda

--- CafeSEM/CafeSEM/clientes/models.py
+++ CafeSEM/CafeSEM/clientes/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+import cx_Oracle
 
-# Create your models here.
+class Producto:
+    def __init__(self):
+        self.connection = cx_Oracle.connect("system", "pythonoracle", "localhost/XE")
+
+    def listadoProductos(self):
+        cursor = self.connection.cursor()
+
+        try:
+            consulta = "SELECT ID_PRODUCTO, NOMBRE, PRECIO, STOCK, IMAGEN  FROM PRODUCTOS"
+            cursor.execute(consulta)
+
+        except self.connection.Error as error:
+            print("Error: ", error)
+
+        return cursor
