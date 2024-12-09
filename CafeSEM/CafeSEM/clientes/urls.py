e77669674d47f7b7eeb68b7d084562e689970d4d
Eva Gómez <88188128+mogaves@users.noreply.github.com>
2024-12-09 10:10:11 +0100

    Update urls.py
    Modificamos path para ver index y productos

--- CafeSEM/CafeSEM/clientes/urls.py
+++ CafeSEM/CafeSEM/clientes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from clientes import views
 
 urlpatterns=[
-    path('', views.index, name='index'),
+    path('',views.index,name='index'),
+    path('productos', views.listadoProductos, name='listadoProductos')
 ]
